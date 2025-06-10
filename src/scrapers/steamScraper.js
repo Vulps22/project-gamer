58f1af9ac045010acaf78fa49b0730aa44feba08
Vulps <ajmcallister5@gmail.com>
2025-06-11 00:00:05 +0100

    Refine steam Scraper

--- src/scrapers/steamScraper.js
+++ src/scrapers/steamScraper.js
@@ -8,30 +8,16 @@
  */
 async function scrape($, url) {
     const data = {
-        title: $('.apphub_AppName').text().trim() || null,
-        description: $('.game_description_snippet').text().trim() || null,
-        imageUrl: $('.game_header_image_full').attr('src') || null,
-        developer: null,
-        publisher: null,
+        title: $('#appHubAppName').text().trim() || null,
         storeSpecificId: null,
     };
 
-    $('.dev_row').each((i, el) => {
-        const row = $(el);
-        const titleEl = row.find('.subtitle').text().trim();
-        const valueEl = row.find('a').text().trim();
-        if (titleEl === 'Developer:') {
-            data.developer = valueEl || null;
-        } else if (titleEl === 'Publisher:') {
-            data.publisher = valueEl || null;
-        }
-    });
+    console.log("Steam Scraper: Scrapped:", data);
 
     const appIdMatch = url.match(/app\/(\d+)/);
     if (appIdMatch && appIdMatch[1]) {
-        data.storeSpecificId = appIdMatch[1];
+        data.storeGameId = appIdMatch[1];
     }
-
     return data;
 }
 
