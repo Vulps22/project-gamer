efcb9d5083e8dc661b97a44ef2a6b733645bfa90
Vulps <ajmcallister5@gmail.com>
2025-06-13 18:45:10 +0100

    Add games to user library from "I have this" button

--- src/buttons/addGameButton.js
+++ src/buttons/addGameButton.js
@@ -1,5 +1,6 @@
 // At the top of your file
 
+const { chooseStoresMessage } = require('../messages/chooseStoresMessage');
 const { gameManager } = require('../services/GameManagerService');
 const { BotButtonInteraction } = require('../structures');
 
@@ -14,11 +15,9 @@ module.exports = {
         console.log("LFG Add Game Button Interaction:", interaction.buttonData, "User ID:", interaction.user.id);
         const gameId = interaction.params.get('id');
         const userId = interaction.user.id;
-        const didAdd = gameManager.addGameToUserLibrary(gameId, userId);
-        if (didAdd) {
-            return interaction.ephemeralReply({ content: 'Game added to your library!', ephemeral: true });
-        } else {
-            return interaction.ephemeralReply({ content: 'You already have this game in your library.', ephemeral: true });
-        }
+        const stores = await gameManager.getStoresForGame(gameId, userId);
+        const message = chooseStoresMessage(stores)
+        await interaction.ephemeralReply(null, message);
+
     },
 };
