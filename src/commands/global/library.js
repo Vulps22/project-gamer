c4bb0eef944ec782c42ffad85b781e7234754dec
Vulps <ajmcallister5@gmail.com>
2025-06-23 20:07:41 +0100

    Fix broken autocomplete in library command

--- src/commands/global/library.js
+++ src/commands/global/library.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder, SlashCommandStringOption, MessageFlags, SlashCommandSubcommandBuilder } = require('discord.js');
-const { gameManager } = require('../../services');
+const { gameManagerService } = require('../../services');
 const { chooseStoresMessage, GlobalMessages } = require('../../messages');
 const { BotInteraction } = require('../../structures');
+const { AutocompleteInteraction } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -36,15 +37,26 @@ module.exports = {
      */
     async autoComplete(interaction) {
         const name = interaction.options.getFocused();
-        const games = await gameManager.searchGamesByName(name);
 
-        //console.log('Autocomplete games:', games);
+        let games = [];
+
+        if (interaction.options.getSubcommand() === 'remove' || interaction.options.getSubcommand() === 'view') {
+            games = await gameManagerService.searchUserGamesByName(name, interaction.user.id);
+        }
+        else {
+            games = await gameManagerService.searchGamesByName(name);
+        }
+
+        console.log('Autocomplete games:', games);
 
         if (!games || games.length === 0) {
             return interaction.respond([{ name: 'No games found', value: 'none' }]);
         }
 
-        interaction.respond(games.map(game => ({ name: game.name, value: String(game.id) })));
+        const choices = games.map(game => ({ name: game.name, value: String(game.id) }))
+        console.log('Autocomplete choices:', choices);
+
+        interaction.respond(choices);
 
     },
 
@@ -55,11 +67,11 @@ module.exports = {
     async execute(interaction) {
         const game = interaction.options.getString('game');
         const isDeleting = interaction.options.getSubcommand() === 'remove';
-        let stores = await gameManager.getStoresForGame(game, interaction.user.id);
+        let stores = await gameManagerService.getStoresForGame(game, interaction.user.id);
 
         if (isDeleting) {
             stores = [];
-            stores = await gameManager.getAllStoresForGame(game);
+            stores = await gameManagerService.getAllStoresForGame(game);
         }
 
         const storesMessage = chooseStoresMessage(stores, isDeleting)
